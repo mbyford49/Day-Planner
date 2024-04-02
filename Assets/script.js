cb2cfe915360ca6208ef0a81a38e1b0b3fc2f1e3
mbyford49 <mbyford49@gmail.com>
2024-04-01 23:24:37 -0700

    final code, readme still needed

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 function updateTime() {
   const currentTime = document.getElementById('currentTime');
 const now = dayjs();
@@ -43,23 +41,36 @@ function setTimeClass() {
 
 setTimeClass();
 setInterval(setTimeClass, 60000);
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+const saveButtons = document.querySelectorAll(".saveBtn");
+
+saveButtons.forEach(button => {
+  button.addEventListener("click", function() {
+  const dataToSave = [];
+
+  for (let i = 0; i <= 8; i++) {
+    const inputField = document.getElementById("hour" + i);
+    const textToSave = inputField.value;
+    console.log(textToSave);
+
+    dataToSave.push(textToSave);
+  }
+
+    const jsonData = JSON.stringify(dataToSave);
+    localStorage.setItem("savedData", jsonData);
+  });
 });
+
+window.onload = function() {
+  const savedDataString = localStorage.getItem("savedData");
+  if (savedDataString) {
+    const savedData = JSON.parse(savedDataString);
+
+    for (let i = 0; i <= 8; i++) {
+      const element = document.getElementById("hour" + i);
+      if (element) {
+        element.value = savedData[i];
+      }
+    }
+  }
+};
